b14b43423019d8c51a9fd6b876a3f2cbe37c0852
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-19 19:59:46 -0700

    connected the routing file to the server file

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+var app = module.exports = express();
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
