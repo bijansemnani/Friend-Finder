65a30e069e3dff0ca83a14dc80cc1e7ca4601455
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-20 10:04:55 -0700

    moved some routing stuff around

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,17 +10,3 @@ app.get("/survey", function(req, res) {
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
-
-app.post("/api/friends", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body-parser middleware
-  var newFriend = req.body;
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-
-  console.log(newFriend);
-
-  friends.push(newFriend);
-
-  res.json(newFriend);
-});
