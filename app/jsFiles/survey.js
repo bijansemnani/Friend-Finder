0e03407f6d6a63fed4b95c846c72bececee95c2a
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-20 12:15:59 -0700

    survey page is ready to go

--- app/jsFiles/survey.js
+++ app/jsFiles/survey.js
@@ -1,11 +1,36 @@
 $(document).ready(function () {
   console.log("here");
 
+  function reset() {
+    $("#name").val(" ");
+    $("#photo").val(" ");
+    for (var i = 1; i < 11; i++) {
+      var q = "#q" + i;
+      $(q).val('').trigger("chosen:updated");
+    }
+  }
+
   $("#submit").on("click", function () {
-    console.log("here");
     var newFriend = {
       name: $("#name").val().trim(),
-      photo: $("#photo").val().trim()
-    }
+      photo: $("#photo").val().trim(),
+      scores:[
+        $("#q1").val().trim(),
+        $("#q2").val().trim(),
+        $("#q3").val().trim(),
+        $("#q4").val().trim(),
+        $("#q5").val().trim(),
+        $("#q6").val().trim(),
+        $("#q7").val().trim(),
+        $("#q8").val().trim(),
+        $("#q9").val().trim(),
+        $("#q10").val().trim()
+      ]
+    };
+    reset();
+    $.post("/api/friends", newFriend)
+    .then(function (data) {
+      console.log(data);
+    })
   });
 });
