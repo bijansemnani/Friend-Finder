a9a7750cafadf0deb2f6f22d8fcd13b896cdf61b
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-19 23:55:01 -0700

    added a post request on the back end for new friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,6 @@ var app = module.exports = express();
 var friends = require("./../data/friends.js");
 
 app.get("/api/friends", function(req, res) {
-  console.log(friends);
   //var chosen = req.params.characters;
   return res.json(friends);
 });
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var app = module.exports = express();
+var friends = require("./../data/friends.js");
 
 app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/survey.html"));
@@ -8,4 +9,18 @@ app.get("/survey", function(req, res) {
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.post("/api/friends", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newFriend = req.body;
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+
+  console.log(newFriend);
+
+  friends.push(newFriend);
+
+  res.json(newFriend);
 });
